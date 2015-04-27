4713590264c7e9a080a291c6663a135eb972e12d
Yashprit Singh <yashprits@gmail.com>
2015-04-28 01:29:40 +0530

    intial code

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 'use strict';
-module.exports = function() {
 
-};
+var childProcess = require("child_process");
+
+var showWIFITower = function(cb) {
+  var os = process.platform;
+
+  switch (os) {
+    case 'darwin':
+      return darwinTower(cb);
+    default:
+      return cb("not support opreating system");
+  }
+}
+
+var renderOutput = function() {
+
+}
+
+var darwinTower = function() {
+
+}
+
+var debianTower = function() {
+
+}
+
+var windowTower = function() {
+
+}
+
+
+
+module.exports = showWIFI
